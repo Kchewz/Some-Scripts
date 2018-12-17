2045e193be581b6abb4db4fd2cf67b89307ca586
Kenneth Chyzewski <KChyzewski@gmail.com>
2018-12-17 10:25:17 -0500

    Update daily.sh

--- daily.sh
+++ daily.sh
@@ -3,7 +3,7 @@
 cd
 cd "/Users/Kchewz/desktop/github/kchewz.github.io"
 i=0
-for DAYS in $(seq 2032 3705); do #Saturday December 15, 2018
+for DAYS in $(seq 2032 3705); do
         COMMITS=$((1 + RANDOM % 1))
         while [ $i -ne $COMMITS ]; do
                 echo " " >> README.md
